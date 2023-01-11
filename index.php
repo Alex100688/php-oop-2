847a4e4f133155b6c60e14ca592c5e2ebc9c2b64
Alex100688 <alessiozingaro2@gmail.com>
2023-01-12 00:51:50 +0100

    stampato il nome del prodotto

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
             <div class="row">
                 <div class="col-4">
                     <div class="card">
-                        <img src="<?php echo $production -> getImage() ?> " class="card-img-top" alt="...">
+                        <img src="<?php echo $production -> getImage() ?> " class="card-img-top" alt="<?php echo $production -> getName()?>">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title"><?php echo $production -> getName()?></h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
